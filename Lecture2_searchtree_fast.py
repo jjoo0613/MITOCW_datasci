c3fe533b86a98aa887ca509b2612db7eb2d018cb
Justin Joo <78771808+jjoo0613@users.noreply.github.com>
2021-07-20 12:01:52 +0900

    Create Lecture2_searchtree_fast.py

---
+++ Lecture2_searchtree_fast.py
@@ -0,0 +1,56 @@
+import random
+import Lecture2_searchtree_menu.py
+
+def buildLargeMenu(numItems, maxVal, maxCost):
+    items = []
+    for i in range(numItems):
+        items.append(Food(str(i),
+                          random.randint(1, maxVal),
+                          random.randint(1, maxCost)))
+    return items
+
+#for numItems in (5, 10, 15, 20, 25, 30, 35, 40, 45):
+#    print('Try a menu with', numItems, 'items')
+#    items = buildLargeMenu(numItems, 90, 250)
+#    testMaxVal(items, 750, False)  
+
+def fastMaxVal(toConsider, avail, memo = {}):
+    """Assumes toConsider a list of subjects, avail a weight
+         memo supplied by recursive calls
+       Returns a tuple of the total value of a solution to the
+         0/1 knapsack problem and the subjects of that solution"""
+    if (len(toConsider), avail) in memo:
+        result = memo[(len(toConsider), avail)]
+    elif toConsider == [] or avail == 0:
+        result = (0, ())
+    elif toConsider[0].getCost() > avail: #아까랑 동일 , 허나 메모라는 객체 추가됨 
+        #Explore right branch only
+        result = fastMaxVal(toConsider[1:], avail, memo)
+    else:
+        nextItem = toConsider[0]
+        #Explore left branch
+        withVal, withToTake =\
+                 fastMaxVal(toConsider[1:], avail - nextItem.getCost(), memo)
+        withVal += nextItem.getValue()
+        #Explore right branch
+        withoutVal, withoutToTake = fastMaxVal(toConsider[1:],avail, memo)
+        #Choose better branch
+        if withVal > withoutVal:
+            result = (withVal, withToTake + (nextItem,))
+        else:
+            result = (withoutVal, withoutToTake)
+    memo[(len(toConsider), avail)] = result #메모에 해당 RESULT 튜플 형태로 추가
+    return result
+
+def testMaxVal(foods, maxUnits, algorithm, printItems = True):
+    print('Menu contains', len(foods), 'items') #결국은 numItem 크기의 buildlargeMenu
+    print('Use search tree to allocate', maxUnits,'calories')
+    val, taken = algorithm(foods, maxUnits)
+    if printItems:
+        print('Total value of items taken =', val)
+        for item in taken:
+            print('   ', item)
+          
+#for numItems in (5, 10, 15, 20, 25, 30, 35, 40, 45, 50):
+#    items = buildLargeMenu(numItems, 90, 250)
+#    testMaxVal(items, 750, fastMaxVal, True)
