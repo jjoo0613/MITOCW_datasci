bfbdec6a9f8a59181b832f0dcb7e4e7c8deeccb4
Justin Joo <78771808+jjoo0613@users.noreply.github.com>
2021-07-22 00:11:59 +0900

    Create Lecture5_RandomWalk.py

---
+++ Lecture5_RandomWalk.py
@@ -0,0 +1,137 @@
+import random, pylab
+
+class Location(object):
+    def __init__(self, x, y)
+        self.x = x
+        self.y = y
+
+    def move(self, deltaX, deltaY):
+        return Location(self.x + deltaX, self.y + deltaY)
+
+    def getX(self):
+        return self.x
+    def getY(self):
+        return self.y
+
+    def distFrom(self, other):
+        xDist = self.x - other.getX()
+        yDist = self.y - other.getY()
+        return (xDist**2 + yDist**2)**0.5
+
+    def __str__(self):
+        return '<' + str(self.x) + ', ' + str(self.y) + '>'
+
+class Field(object):
+    def __init__(self):
+        self.drunks = {}
+    def addDrunk(self, drunk, loc):
+        if drunk in self.drunks:
+            raise ValueError('Duplicate drunk')
+        else:
+            self.drunks[drunk] = loc
+            
+    def moveDrunk(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in field')
+        xDist, yDist = drunk.takeStep()
+        #use move method of Location to get new location
+        self.drunks[drunk] =\
+            self.drunks[drunk].move(xDist, yDist)
+        
+    def getLoc(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in field')
+        return self.drunks[drunk]
+
+class Drunk(object):
+    def __init__(self, name = None):
+        """Assumes name is a str"""
+        self.name = name
+
+    def __str__(self):
+        if self != None:
+            return self.name
+        return 'Anonymous'
+
+class UsualDrunk(Drunk):
+    def takeStep(self):
+        stepChoices = [(0,1), (0,-1), (1, 0), (-1, 0)]
+        return random.choice(stepChoices)
+
+class MasochistDrunk(Drunk):
+    def takeStep(self):
+        stepChoices = [(0.0,1.1), (0.0,-0.9),
+                       (1.0, 0.0), (-1.0, 0.0)]
+        return random.choice(stepChoices)
+
+def walk(f, d, numSteps):
+    """Assumes: f a Field, d a Drunk in f, and numSteps an int >= 0.
+       Moves d numSteps times, and returns the distance between
+       the final location and the location at the start of the 
+       walk."""
+    start = f.getLoc(d)
+    for s in range(numSteps):
+        f.moveDrunk(d)
+    return start.distFrom(f.getLoc(d))
+    
+def simWalks(numSteps, numTrials, dClass):
+    Homer = dClass('Homer')
+    origin = Location(0, 0)
+    distances = []
+    for t in range(numTrials):
+        f = Field()
+        f.addDrunk(Homer, origin)
+        distances.append(round(walk(f, Homer, numSteps ), 1))   #originally numTrials -> error in drunktest! 
+    return distances
+
+def drunkTest(walkLengths, numTrials, dClass):
+    """Assumes walkLengths a sequence of ints >= 0
+         numTrials an int > 0, dClass a subclass of Drunk
+       For each number of steps in walkLengths, runs simWalks with
+         numTrials walks and prints results"""
+    for numSteps in walkLengths:
+        distances = simWalks(numSteps, numTrials, dClass)
+        print(dClass.__name__, 'random walk of', numSteps, 'steps')
+        print(' Mean =', round(sum(distances)/len(distances), 4))
+        print(' Max =', max(distances), 'Min =', min(distances))
+        
+random.seed(0)
+drunkTest((10, 100, 1000, 10000), 100, UsualDrunk)
+#
+def simAll(drunkKinds, walkLengths, numTrials):
+    for dClass in drunkKinds:
+        drunkTest(walkLengths, numTrials, dClass)
+        
+#random.seed(0)
+#simAll((UsualDrunk, MasochistDrunk),
+#       (1000, 10000), 100)
+#        
+#xVals = [1, 2, 3, 4]
+#yVals1 = [1, 2, 3, 4]
+#pylab.plot(xVals, yVals1, 'b-', label = 'first')
+#yVals2 = [1, 7, 3, 5]
+#pylab.plot(xVals, yVals2, 'r--', label = 'second')
+#pylab.legend()
+
+class styleIterator(object):
+    def __init__(self, styles):
+        self.index = 0
+        self.styles = styles
+
+    def nextStyle(self):
+        result = self.styles[self.index]
+        if self.index == len(self.styles) - 1:
+            self.index = 0
+        else:
+            self.index += 1
+        return result
+    
+def simDrunk(numTrials, dClass, walkLengths):
+    meanDistances = []
+    for numSteps in walkLengths:
+        print('Starting simulation of',
+              numSteps, 'steps')
+        trials = simWalks(numSteps, numTrials, dClass)
+        mean = sum(trials)/len(trials)
+        meanDistances.append(mean)
+    return meanDistances
