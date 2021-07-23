b5ad6223059c0c73dd33612526641afa17e0673a
Justin Joo <78771808+jjoo0613@users.noreply.github.com>
2021-07-23 14:21:29 +0900

    Update Lecture6_Roulette.py

--- Lecture6_Roulette.py
+++ Lecture6_Roulette.py
@@ -35,5 +35,42 @@ game = FairRoulette()
 for numSpins in (100, 1000000):
     for i in range(3):
         playRoulette(game, numSpins, 2,1, True) 
+
+####    append gametype - Eu, Am are subclass of FairRoulette- they function the same as FairRoulette! 
+class EuRoulette(FairRoulette): #Eu Roulette is a subclass of FairRoulette
+    def __init__(self):
+        FairRoulette.__init__(self)
+        self.pockets.append('0') #European pocket
+    def __str__(self):
+        return "European Roulette"
+
+class AmRoulette(EuRoulette): #American Roulette is subclass of EU roulette < FairRoulette
+    def __init__(self):
+        EuRoulette.__init__(self)
+        self.pockets.append('00') #another pocket --> thus less likely
+    def __str__(self):
+        return 'American Roullete'
+
+####Simulations.. . 
     
-    
+def findPocketReturn(game, numTrials, trialSize, toPrint):
+    pocketReturns = []
+    for t in range(numTrials):
+        trialVals = playRoulette(game, trialSize, 2, 1, toPrint)
+        pocketReturns.append(trialVals)
+    return pocketReturns
+
+random.seed(0)
+numTrials = 20
+resultDict = {}
+games = (FairRoulette, EuRoulette, AmRoulette)
+for gametype in games:
+    resultDict[gametype().__str__()] = [] #since was in tuple - we hav eto extract w/ .__str__() .... 
+                                   # we need to G() with this bracket for the input of class(self) 
+for numSpins in (1000, 10000, 100000, 1000000):
+    print('\nSimulate', numTrials, 'trials of', numSpins, 'spins each')
+    for gametype in games:
+        pocketReturns = findPocketReturn(gametype(), numTrials,numSpins, False)
+        expReturn = 100*sum(pocketReturns)/len(pocketReturns)
+        print('Exp. return for', gametype(), '=', str(round(expReturn, 3)) + '%')
+             
