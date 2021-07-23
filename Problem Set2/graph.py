4cafb8041aa0d27eb5e12b018f1b5160b516e349
Justin Joo <78771808+jjoo0613@users.noreply.github.com>
2021-07-23 15:24:34 +0900

    Update graph.py

--- Problem Set2/graph.py
+++ Problem Set2/graph.py
@@ -14,65 +14,53 @@ class Node(object):
     """Represents a node in the graph"""
     def __init__(self, name):
         self.name = str(name)
-
     def get_name(self):
         return self.name
-
     def __str__(self):
         return self.name
-
-    def __repr__(self):
+    def __repr__(self):  #??????
         return self.name
-
     def __eq__(self, other):
         return self.name == other.name
-
-    def __ne__(self, other):
+    def __ne__(self, other):   #notequal
         return not self.__eq__(other)
-
     def __hash__(self):
-        # This function is necessary so that Nodes can be used as
-        # keys in a dictionary, even though Nodes are mutable
+        # This function is necessary so that Nodes can be used as keys in a dictionary, even though Nodes are mutable
         return self.name.__hash__()
 
 
 class Edge(object):
-    """Represents an edge in the dictionary. Includes a source and
-    a destination."""
+    """Represents an edge in the dictionary. Includes a source and a destination."""
     def __init__(self, src, dest):
         self.src = src
         self.dest = dest
-
     def get_source(self):
         return self.src
-
     def get_destination(self):
         return self.dest
-
     def __str__(self):
         return '{}->{}'.format(self.src, self.dest)
 
 
 class WeightedEdge(Edge):
     def __init__(self, src, dest, total_distance, outdoor_distance):
+        self.src = src
+        self.dest = dest
+        self.total_distance = total_distance
+        self.outdoor_distance = outdoor_distance
         pass  # TODO
-
     def get_total_distance(self):
         pass  # TODO
-
     def get_outdoor_distance(self):
         pass  # TODO
-
     def __str__(self):
         pass  # TODO
-
 
 class Digraph(object):
     """Represents a directed graph of Node and Edge objects"""
     def __init__(self):
         self.nodes = set([])
         self.edges = {}  # must be a dict of Node -> list of edges
-
     def __str__(self):
         edge_strs = []
         for edges in self.edges.values():
@@ -83,15 +71,12 @@ class Digraph(object):
 
     def get_edges_for_node(self, node):
         return self.edges[node]
-
     def has_node(self, node):
         return node in self.nodes
-
     def add_node(self, node):
         """Adds a Node object to the Digraph. Raises a ValueError if it is
         already in the graph."""
         pass  # TODO
-
     def add_edge(self, edge):
         """Adds an Edge or WeightedEdge instance to the Digraph. Raises a
         ValueError if either of the nodes associated with the edge is not
